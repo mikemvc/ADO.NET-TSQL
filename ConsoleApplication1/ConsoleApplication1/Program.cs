e90fa5db65e4a5566080fc6dd67aeb5ae205341f
mikemvc <mikemvc@hotmail.com>
2015-07-29 14:48:51 +0800

    select1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string connectionString = @"Data Source=(localdb)\v11.0;attachdbfilename=|DataDirectory|\NorthwindDB.mdf;;Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connectionString);
+            string sql = "";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            #region 查詢
+            #region 單一資料表查詢
+            cmd.CommandText = "select * from customers";
+            conn.Open();
+            var result = cmd.ExecuteReader();
+            while(result.Read())
+            {
+                Console.WriteLine(result[0]);
+            }
+            #endregion
+            #endregion
+
+            conn.Close();
+            Console.Read();
         }
     }
 }
